8bb9a475bea00fe076565c18465856a053330dae
Raul Oviedo <antoniorah@gmail.com>
2019-04-02 00:10:25 -0400

    Updated README

--- Main.js
+++ Main.js
@@ -35,21 +35,6 @@ export default class Main extends React.Component {
     }
   }
 
-  // getImage = (name) => {
-  //   switch (name) {
-  //     case 'monkey1': return require('./monkey1.jpg');
-  //     case 'monkey2': return require('./monkey2.jpg');
-  //     case 'monkey3': return require('./monkey3.jpg');
-  //     case 'monkey4': return require('./monkey4.jpg');
-  //     case 'monkey5': return require('./monkey5.jpg');
-  //     case 'monkey6': return require('./monkey6.jpg');
-  //     case 'monkey7': return require('./monkey7.jpg');
-  //     case 'monkey8': return require('./monkey8.jpg');
-  //     case 'monkey9': return require('./monkey9.jpg');
-  //     case 'monkey10': return require('./monkey10.jpg');
-  //   }
-  // }
-
   tryAgain = () => {
     this.setState({
       index: 0,
